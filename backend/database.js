0b127851cba05e9b055e011ad2df214a50b9aff8
khkhle <khang.pan.le@gmail.com>
2025-03-06 00:50:35 +0200

    add links to news in home page

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ import { postgres } from "./deps.js";
 export const sql = postgres({
     user: 'postgres',
     database: 'postgres',
-    password: 'your password',
+    password: '3angiangLX1#',
     hostname: 'localhost',
     port: 8000,
     max: 2
